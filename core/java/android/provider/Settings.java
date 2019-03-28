fc31818c8c090a05fe78fb81175b07980e9a9a3b
Lucchetto <lucchetto.tie@live.com>
2019-03-28 18:41:03 +0100

    base: Smart Charging (1/4)
    Change-Id: I7a297a6389e7f4f6b0dfd20a958ebbd8e07befd9

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4520,6 +4520,16 @@ public final class Settings {
           */
         public static final String VOLUME_KEY_CURSOR_CONTROL = "volume_key_cursor_control";
 
+	/**
+         * @hide
+         */
+        public static final String SMART_CHARGING = "smart_charging";
+
+        /**
+         * @hide
+         */
+        public static final String SMART_CHARGING_LEVEL = "smart_charging_level";
+
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
